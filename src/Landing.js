3d63dcca105fa5685f0f187a0a71f9a719c529d4
Annette Coll <annette.coll@outlook.com>
2018-01-29 12:08:45 -0500

    setup of landing.js view

---
+++ src/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>litswap</h1>
+            <h2>Your swapping dreams!</h2>
+            <button className='enter-button'>Enter</button>
+        </div>
+    ) 
+}
+
+export default Landing
