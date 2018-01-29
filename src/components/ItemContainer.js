1923f7f4f5671ae04a333aa1e13c808aeb78e19f
Annette Coll <annette.coll@outlook.com>
2018-01-29 16:02:53 -0500

    item container

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
 import {CLIENT_URL } from '../constants'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
-
+class ItemContainer extends Component {
+  render () {
+    return (
+      <div>Hello Container</div>
+    ) 
+  }
+}
 
 
 
