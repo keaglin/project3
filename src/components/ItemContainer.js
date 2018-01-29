59f356829116490c00456a3c63f501f781431b8b
Annette Coll <annette.coll@outlook.com>
2018-01-29 15:59:03 -0500

    set up constants

---
+++ src/components/ItemContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import {CLIENT_URL } from '../constants'
+
+
+
+
+
+
+
+export default ItemContainer
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const CLIENT_URL = 'https://whispering-brook-24620.herokuapp.com/people'
