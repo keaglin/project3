e5e6c2a79e85c59d56416cc018726575529bb672
K. Eaglin <kceaglin@gmail.com>
2018-01-30 10:57:21 -0500

    edits save but redirect not yet functional

--- src/components/ItemEdit.js
+++ src/components/ItemEdit.js
@@ -1,33 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios                from 'axios'
+import { CLIENT_URL }       from '../constants'
 
-const ItemEdit extends Component {
+class ItemEdit extends Component {
   state = {
     book: this.props.location.state.book
   }
 
-  handleTitle() {
-    this.setState({ book: {title: e.target.value} })
+  // Help with Object.assign for setting state on nested props from Stack Overflow
+  // it was also mentioned in a lesson but SO helped me understand how to implement it
+  // https://stackoverflow.com/questions/43040721/how-to-set-a-nested-state-in-react
+  handleTitle = (e) => {
+    // if (e.target.value) this.setState({book: {title: e.target.value}})
+    if (e.target.value) this.setState({book: Object.assign(this.state.book, {title: e.target.value})})
   }
-  handleAuthor() {
-    this.setState({ book: {author: e.target.value} })
+  handleAuthor = (e) => {
+    // console.log('book from handleAuthor is', this.state.book)
+    // if (e.target.value) this.setState({book: {author: e.target.value}})
+    if (e.target.value) this.setState({book: Object.assign(this.state.book, {author: e.target.value})})
   }
-  handleQuality() {
-    this.setState({ book: {quality: e.target.value} })
+  handleQuality = (e) => {
+    // if (e.target.value) this.setState({book: {quality: e.target.value}})
+    if (e.target.value) this.setState({book: Object.assign(this.state.book, {quality: e.target.value})})
   }
-  handleQuote() {
-    this.setState({ book: {quote: e.target.value} })
+  handleQuote = (e) => {
+    if (e.target.value) this.setState({book: Object.assign(this.state.book, {quote: e.target.value})})
   }
-  handleOwner() {
-    this.setState({ book: {owner: e.target.value} })
+  handleOwner = (e) => {
+    // if (e.target.value) this.setState({book: {owner: e.target.value}})
+    if (e.target.value) this.setState({book: Object.assign(this.state.book, {owner: e.target.value})})
   }
 
-  handleSubmit() {
-    axios.put(`${CLIENT_URL}`)
-      .then()
+  // Got help with axios from here:
+  // https://github.com/axios/axios
+  // and here:
+  // https://medium.com/codingthesmartway-com-blog/getting-started-with-axios-166cb0035237
+  handleSubmit = () => {
+    // console.log(CLIENT_URL)
+    axios.put(`${CLIENT_URL}/${this.state.book.title}`, {book: this.state.book})
+      // .then(response => response.redirect(`/books`))
+      .then(
+        // console.log('book from axios.get.then is', this.state.book)
+        axios.get(`/books/${this.state.book.title}`)
+      )
+      .catch(err => console.log('Woops!', err))
   }
 
   render() {
-    // let book = this.props.location.state.book
+    let book = this.state.book
+    console.log('book is', book)
     return(
       <div>
         <h1>{book.title}</h1>
