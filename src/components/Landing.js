36b68881c64b2d31e6714c16aab9d0fef7451203
Annette Coll <annette.coll@outlook.com>
2018-01-30 10:52:46 -0500

    landing page style changes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,7 @@ const Landing = () => {
     return (
         <div>
             <h1 className='title'>litswap</h1>
-            <h2 className ='header'>Swap your used books to discover new adventures.</h2>
+            <h2 className ='header'>Swap used books to discover new adventures.</h2>
             <a className='login-signin-btn' href='/users'>Login / SignIn</a>
             {/* <button className='enter-button'>Enter</button> */}
         </div>
