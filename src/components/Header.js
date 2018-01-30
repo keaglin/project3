b8a7fdb59af6acac11d9d43ecf298bf2eb2847f5
Annette Coll <annette.coll@outlook.com>
2018-01-30 09:38:35 -0500

    organizing css folders + set up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import '../css/Header.css'
+
 
 const Header = () => {
   return(
     <header>
       <nav>
-        <div className="nav-wrapper cyan darken-1">
-          <Link to="/" className="brand-logo center">LitSwap!</Link>
+        <div className="nav-wrapper #5C351B brown darken-2">
+          <Link to="/" className="brand-logo-center">litswap</Link>
           <ul id="nav-mobile" className="left hide-on-med-and-down">
-            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">HOME</Link></li>
             <li><Link to="/books">Books</Link></li>
             <li><Link to="/users">Users</Link></li>
           </ul>
