e248637b28cbe4bbff8e1d130191e8549ab2677f
Annette Coll <annette.coll@outlook.com>
2018-02-02 11:28:24 -0500

    grid layout of records

--- src/components/RecordList.js
+++ src/components/RecordList.js
@@ -1,20 +1,21 @@
 import React  from 'react'
 import {Link} from 'react-router-dom'
+import '../css/RecordList.css'
 
 const RecordList = (props) => {
   const records = props.records
   console.log(records)
   return (
-    <div>
-      <ul>
+    <div className='recrow'>
+      <ul className='record-list-container'>
         {
           records.map((record, index) => {
             return (
-              <Link to={{
+              <Link className='reclink' to={{
                 pathname: `/records/${record.name}`,
                 state: {record: record}
               }}> 
-                <li className='list-li' key={index}>
+                <li className='rec-list-li' key={index}>
                   <div className='image-wrap'>
                     <img className='img-item-list image' src={record.imageUrl} alt="cover"/>
                   </div>
